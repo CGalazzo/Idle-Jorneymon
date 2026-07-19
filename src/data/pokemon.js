b8aef452eab25372df029a8a3cbabe2ced3523c4
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-19 17:00:01 -0300

    feat: update src/data/pokemon.js for v0.2.0

--- src/data/pokemon.js
+++ src/data/pokemon.js
@@ -1,24 +1,37 @@
 const SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated";
 
-export const PLAYER_TEMPLATE = {
-  id: 4,
-  name: "Charmander",
-  level: 5,
-  maxHp: 29,
-  attack: 12,
-  defense: 8,
-  xp: 0,
-  xpToNext: 30,
-  sprite: `${SPRITE_BASE}/4.gif`,
-  backSprite: `${SPRITE_BASE}/back/4.gif`
-};
+export const STARTERS = [
+  { id: 1, name: "Bulbasaur", type: "Planta", level: 5, maxHp: 31, attack: 11, defense: 10 },
+  { id: 4, name: "Charmander", type: "Fogo", level: 5, maxHp: 29, attack: 12, defense: 8 },
+  { id: 7, name: "Squirtle", type: "Água", level: 5, maxHp: 32, attack: 10, defense: 11 }
+].map(withSprites);
 
 export const ROUTE_ONE_ENCOUNTERS = [
-  { id: 19, name: "Rattata", baseHp: 18, attack: 8, defense: 6, xp: 12, sprite: `${SPRITE_BASE}/19.gif` },
-  { id: 16, name: "Pidgey", baseHp: 20, attack: 8, defense: 7, xp: 13, sprite: `${SPRITE_BASE}/16.gif` },
-  { id: 10, name: "Caterpie", baseHp: 22, attack: 7, defense: 8, xp: 11, sprite: `${SPRITE_BASE}/10.gif` },
-  { id: 13, name: "Weedle", baseHp: 20, attack: 8, defense: 7, xp: 12, sprite: `${SPRITE_BASE}/13.gif` }
-];
+  { id: 19, name: "Rattata", type: "Normal", baseHp: 18, attack: 8, defense: 6, xp: 12 },
+  { id: 16, name: "Pidgey", type: "Normal/Voador", baseHp: 20, attack: 8, defense: 7, xp: 13 },
+  { id: 10, name: "Caterpie", type: "Inseto", baseHp: 22, attack: 7, defense: 8, xp: 11 },
+  { id: 13, name: "Weedle", type: "Inseto/Veneno", baseHp: 20, attack: 8, defense: 7, xp: 12 }
+].map(withSprites);
+
+export const POKEDEX_SPECIES = [...STARTERS, ...ROUTE_ONE_ENCOUNTERS];
+
+function withSprites(pokemon) {
+  return {
+    ...pokemon,
+    sprite: `${SPRITE_BASE}/${pokemon.id}.gif`,
+    backSprite: `${SPRITE_BASE}/back/${pokemon.id}.gif`
+  };
+}
+
+export function createStarter(starterId) {
+  const selected = STARTERS.find((pokemon) => pokemon.id === Number(starterId)) || STARTERS[1];
+  return {
+    ...selected,
+    xp: 0,
+    xpToNext: 30,
+    hp: selected.maxHp
+  };
+}
 
 export function createWildPokemon(playerLevel, random = Math.random) {
   const template = ROUTE_ONE_ENCOUNTERS[Math.floor(random() * ROUTE_ONE_ENCOUNTERS.length)];
