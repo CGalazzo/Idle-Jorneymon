9df1eb65491a6e24671144048c71cb7cba34b184
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-19 22:12:23 -0300

    chore: bump smooth encounter fix to v0.6.1

--- src/core/game-state.js
+++ src/core/game-state.js
@@ -1,7 +1,7 @@
 import { createStarter } from "../data/pokemon.js";
 import { createAreaState } from "../data/worlds.js";
 
-export const GAME_VERSION = "0.6.0";
+export const GAME_VERSION = "0.6.1";
 export const SAVE_VERSION = 7;
 export const MAX_TEAM_SIZE = 3;
 
