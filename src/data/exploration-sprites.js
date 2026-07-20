cf5f13cbf7ce4fb47580b97e81fb71f6755b7fc7
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-19 22:45:12 -0300

    fix: restore fully animated exploration sprites

--- src/data/exploration-sprites.js
+++ src/data/exploration-sprites.js
@@ -1,6 +1,6 @@
-const STATIC_SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+const ANIMATED_SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated";
 
 export function getExplorationSpriteUrl(pokemon, forceShiny = Boolean(pokemon?.isShiny)) {
   const speciesId = Math.max(1, Number(pokemon?.id) || 1);
-  return `${STATIC_SPRITE_BASE}/${forceShiny ? "shiny/" : ""}${speciesId}.png`;
+  return `${ANIMATED_SPRITE_BASE}/${forceShiny ? "shiny/" : ""}${speciesId}.gif`;
 }
