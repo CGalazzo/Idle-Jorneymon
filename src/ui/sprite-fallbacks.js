510a183403962884e4335038568efd05f86f43bb
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-20 18:37:11 -0300

    fix: add safe sprite fallbacks for animated forms

---
+++ src/ui/sprite-fallbacks.js
@@ -0,0 +1,33 @@
+const STATIC_SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
+const SHOWDOWN_PATTERN = /\/sprites\/pokemon\/other\/showdown\/(back\/)?(shiny\/)?(\d+)\.gif(?:\?.*)?$/i;
+const STATIC_BACK_PATTERN = /\/sprites\/pokemon\/back\/(shiny\/)?(\d+)\.png(?:\?.*)?$/i;
+
+function staticSpriteUrl(id, { shiny = false, back = false } = {}) {
+  const backPath = back ? "back/" : "";
+  const shinyPath = shiny ? "shiny/" : "";
+  return `${STATIC_SPRITE_BASE}/${backPath}${shinyPath}${id}.png`;
+}
+
+export function installSpriteFallbacks() {
+  document.addEventListener("error", (event) => {
+    const image = event.target;
+    if (!(image instanceof HTMLImageElement)) return;
+
+    const currentUrl = image.currentSrc || image.src || "";
+    const showdownMatch = currentUrl.match(SHOWDOWN_PATTERN);
+    if (showdownMatch) {
+      const isBack = Boolean(showdownMatch[1]);
+      const isShiny = Boolean(showdownMatch[2]);
+      const id = showdownMatch[3];
+      image.dataset.spriteFallbackStage = isBack ? "static-back" : "static-front";
+      image.src = staticSpriteUrl(id, { shiny: isShiny, back: isBack });
+      return;
+    }
+
+    const staticBackMatch = currentUrl.match(STATIC_BACK_PATTERN);
+    if (staticBackMatch && image.dataset.spriteFallbackStage === "static-back") {
+      image.dataset.spriteFallbackStage = "static-front";
+      image.src = staticSpriteUrl(staticBackMatch[2], { shiny: Boolean(staticBackMatch[1]) });
+    }
+  }, true);
+}
