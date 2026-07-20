4482c8c48826b6cc56e3972147a0e5651621da53
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-20 18:34:16 -0300

    fix: animate modern and mega exploration sprites

--- src/data/exploration-sprites.js
+++ src/data/exploration-sprites.js
@@ -1,11 +1,14 @@
 const ANIMATED_SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated";
-const STATIC_SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
-const LAST_ANIMATED_SPECIES_ID = 649;
+const SHOWDOWN_SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown";
+const LAST_GENERATION_FIVE_SPECIES_ID = 649;
 
 export function getExplorationSpriteUrl(pokemon, forceShiny = Boolean(pokemon?.isShiny)) {
-  const speciesId = Math.max(1, Number(pokemon?.id) || 1);
-  if (speciesId <= LAST_ANIMATED_SPECIES_ID) {
-    return `${ANIMATED_SPRITE_BASE}/${forceShiny ? "shiny/" : ""}${speciesId}.gif`;
+  const speciesId = Math.max(1, Number(pokemon?.megaFormId || pokemon?.id) || 1);
+  const shinyPath = forceShiny ? "shiny/" : "";
+
+  if (speciesId <= LAST_GENERATION_FIVE_SPECIES_ID) {
+    return `${ANIMATED_SPRITE_BASE}/${shinyPath}${speciesId}.gif`;
   }
-  return `${STATIC_SPRITE_BASE}/${forceShiny ? "shiny/" : ""}${speciesId}.png`;
+
+  return `${SHOWDOWN_SPRITE_BASE}/${shinyPath}${speciesId}.gif`;
 }
