e2c8baf42031cc118d3d8ef298993fba19bb4f07
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-22 20:52:59 -0300

    feat: include Champions Hall species in Pokedex

--- src/data/pokedex-data.js
+++ src/data/pokedex-data.js
@@ -1,10 +1,12 @@
+import { CHAMPIONS_HALL_SPECIES } from "./champions-hall-data.js";
 import { POKEDEX_SPECIES, getPokemonSpriteUrls } from "./pokemon.js";
 import { SAFARI_SPECIES } from "./safari-data.js";
 
 export const COMPLETE_POKEDEX_SPECIES = [...new Map([
   ...POKEDEX_SPECIES,
-  ...SAFARI_SPECIES.map((pokemon) => ({
-    ...pokemon,
-    ...getPokemonSpriteUrls(pokemon.id, false)
-  }))
-].map((pokemon) => [Number(pokemon.id), pokemon])).values()].sort((a, b) => a.id - b.id);
+  ...SAFARI_SPECIES,
+  ...CHAMPIONS_HALL_SPECIES
+].map((pokemon) => [Number(pokemon.id), {
+  ...pokemon,
+  ...getPokemonSpriteUrls(pokemon.id, false)
+}])).values()].sort((a, b) => a.id - b.id);
