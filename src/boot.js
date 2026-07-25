9f6f932844f2edfa19ade8bbfab6d860d2999b1e
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-25 18:27:41 -0300

    Remove Megas Z sem sprites de batalha compatíveis

--- src/boot.js
+++ src/boot.js
@@ -1,4 +1,15 @@
-import { loadMegaFormData } from "./data/mega-data.js";
+import { loadMegaFormData, MEGA_STONES } from "./data/mega-data.js";
+
+const DISABLED_MEGA_STONE_IDS = new Set([
+  "lucarionite-z",
+  "garchompite-z"
+]);
+
+for (let index = MEGA_STONES.length - 1; index >= 0; index -= 1) {
+  if (DISABLED_MEGA_STONE_IDS.has(MEGA_STONES[index]?.id)) {
+    MEGA_STONES.splice(index, 1);
+  }
+}
 
 loadMegaFormData();
 await import("./main.js");
